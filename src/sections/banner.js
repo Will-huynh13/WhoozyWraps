f00e646c69b16aedebe5eeec5d7b2cf1c2a599eb
Will Huynh <47924673+Will-huynh13@users.noreply.github.com>
2022-06-03 00:21:42 -0600

    Adding service section and text feature

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -2,8 +2,6 @@
 import { jsx } from 'theme-ui';
 import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
 import BannerImg from 'assets/banner-thumb.png';
-import ShapeLeft from 'assets/shape-left.png';
-import ShapeRight from 'assets/shape-right.png';
 
 export default function Banner() {
   return (
@@ -43,7 +41,6 @@ const styles = {
       height: '100%',
       width: '100%',
       zIndex: -1,
-      backgroundImage: `url(${ShapeLeft})`,
       backgroundRepeat: `no-repeat`,
       backgroundPosition: 'bottom left',
       backgroundSize: '36%',
@@ -56,7 +53,6 @@ const styles = {
       height: '100%',
       width: '100%',
       zIndex: -1,
-      backgroundImage: `url(${ShapeRight})`,
       backgroundRepeat: `no-repeat`,
       backgroundPosition: 'bottom right',
       backgroundSize: '32%',
